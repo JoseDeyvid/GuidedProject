3fb61f77650ccdbdca4b91787bc4ad2b052ff015
JoseDeyvid <j_deyvid@live.com>
2026-04-22 12:19:44 -0300

    Add middleware to verify authenticated users

--- finance-api/src/routes/auth.routes.ts
+++ finance-api/src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 const authController = new AuthController();
 
 router.post("/register", (req, res) => authController.register(req, res));
 router.post("/login", (req, res) => authController.login(req, res));
+router.get("/me", authMiddleware, (req, res) => {
+  return res.json({ message: "Access granted" });
+});
 
 export default router;
