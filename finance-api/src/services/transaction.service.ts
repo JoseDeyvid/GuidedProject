0b278af74bd2a0950a23bb2db17ea2f48fb6b1d0
JoseDeyvid <j_deyvid@live.com>
2026-04-27 10:15:41 -0300

    Remove the responsability of calculating the summary from the backend and move it to the database

--- finance-api/src/services/transaction.service.ts
+++ finance-api/src/services/transaction.service.ts
@@ -32,28 +32,66 @@ export class TransactionService {
     return deletedTransaction;
   }
 
-  async summary(userId: string) {
-    const transactions = await prisma.transaction.findMany({
-      where: { userId },
-    });
+  // This commented function makes Node perform the calculation,
+  // and the other makes the database perform the calculation; in this way,
+  // the processing on the backend is reduced, making it faster and more scalable.
 
-    const summary = transactions.reduce(
-      (acc, transaction) => {
-        if (transaction.type === "income") {
-          acc.income += transaction.amount;
-          acc.balance += transaction.amount;
-        } else {
-          acc.expense += transaction.amount;
-          acc.balance -= transaction.amount;
-        }
-        return acc;
-      },
-      {
-        income: 0,
-        expense: 0,
-        balance: 0,
-      },
-    );
-    return summary;
+  // async summary(userId: string) {
+  //   const transactions = await prisma.transaction.findMany({
+  //     where: { userId },
+  //   });
+  //   const summary = transactions.reduce(
+  //     (acc, transaction) => {
+  //       if (transaction.type === "income") {
+  //         acc.income += transaction.amount;
+  //         acc.balance += transaction.amount;
+  //       } else {
+  //         acc.expense += transaction.amount;
+  //         acc.balance -= transaction.amount;
+  //       }
+  //       return acc;
+  //     },
+  //     {
+  //       income: 0,
+  //       expense: 0,
+  //       balance: 0,
+  //     },
+  //   );
+  //   return summary;
+  // }
+
+  // The commented function makes Node perform the calculation,
+  // and that makes the database perform the calculation; in this way,
+  // the processing on the backend is reduced, making it faster and more scalable.
+  async summary(userId: string) {
+    const [income, expense] = await Promise.all([
+      prisma.transaction.aggregate({
+        where: {
+          userId,
+          type: "income",
+        },
+        _sum: {
+          amount: true,
+        },
+      }),
+      prisma.transaction.aggregate({
+        where: {
+          userId,
+          type: "expense",
+        },
+        _sum: {
+          amount: true,
+        },
+      }),
+    ]);
+
+    const totalIncome = income._sum.amount || 0;
+    const totalExpense = expense._sum.amount || 0;
+
+    return {
+      income: totalIncome,
+      expense: totalExpense,
+      balance: totalIncome - totalExpense,
+    };
   }
 }
